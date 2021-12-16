b2904c803703e52192a678225eda96d816767016
Priya Potti <59410993+Priya378@users.noreply.github.com>
2021-12-16 11:51:21 +0530

    Create Source Codes folder and add a program

---
+++ Source Codes/skeleton_code_word_count.java
@@ -0,0 +1,19 @@
+//skeleton of map reduce program
+/*
+import statements
+class Main_class{
+    class mapper_class{
+        map(){
+
+        }
+    }
+    class reducer_class{
+        reduce(){
+
+        }
+    }
+    main(){
+
+    }
+}
+*/
